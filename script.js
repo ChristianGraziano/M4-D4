58b09fedc015e0e5a54cd3a0387c57cab5491814
ChristianGraziano <123503135+ChristianGraziano@users.noreply.github.com>
2023-05-26 19:46:35 +0200

    ..

--- script.js
+++ script.js
@@ -2,13 +2,33 @@
 
 const ApiUrl = "https://striveschool-api.herokuapp.com/books"
 
+let divCard = document.getElementById('board');
+
 const getBook = () => {
-    fetch (ApiUrl)
-    .then((result) => {
-        return result.json()
+    fetch (ApiUrl,)
+    .then((response) => {
+        return response.json()
     })
     .then((json) => {
-        console.log(json.title);
-    })
+        for (let i = 0; i < json.length; i++) {
+            let bookTitle = json[i].title;
+            let bookImg = json[i].img;
+            let bookPrice = json.price;
+
+            let newDiv= document.createElement("div");
+    newDiv.classList.add("col-12", "col-sm-6", "col-md-4","col-lg-3", "col-xl-2","d-flex", "justify-content-center")
+    newDiv.innerHTML=
+    `<div class="card" style="width: 18rem;">
+     <img src="${bookImg}" class="card-img-top" alt="...">
+     <div class="card-body">
+         <h5 class="card-title">${bookTitle}</h5>
+         <p class="card-text">€ ${bookPrice}</p>
+         <button onclick="addCart()" type="button" class="btn btn-primary">Add to cart</button>
+     </div>
+    </div>;`
+    
+    }
+     })
     .catch((error) => { console.log(error);})
 }
+
